a92f5557be8631c9c4e2c77da42e39adf0086644
islam heddi <islamheddi19@gmail.com>
2024-08-06 23:07:33 +0100

    move pdf files

--- backend/router/movefiles.js
+++ backend/router/movefiles.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const router = express.Router()
 const fs = require('fs')
+const path = require('path')
 
 router.post('/move',(req,res) => {
     const { oldpath } = req.body
-    const newpath = "./files/thisfile.txt"
+    const ext = path.extname(oldpath)
+    if(ext != ".pdf") return res.status(400).send("Error : the extension must be pdf")
+    const filename = path.basename(oldpath,'.pdf')
+    var today = new Date()
+    var year = today.getFullYear()
+    var month = today.getMonth()
+    var day = today.getDay()
+    var hour = today.getHours()
+    var minute = today.getMinutes()
+    var second = today.getSeconds()
+    const newpath = `./files/${filename}${year}${month}${day}${hour}${minute}${second}.pdf`
     fs.rename(oldpath,newpath,(err) => {
-        if(err) return res.status(400).send("Error")
+        if(err) return res.status(400).send(`Error : ${err}`)
         res.status(200).send("Sended successfully")
     }) 
 })
