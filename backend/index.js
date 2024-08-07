110d55cf8f5f694e4f69389b2a1bdba6b9f7185a
islam heddi <islamheddi19@gmail.com>
2024-08-07 16:12:35 +0100

    error in mongodb

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,43 @@ const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const movefiles = require('./router/movefiles')
+const MongoClient = require('mongodb').MongoClient
+const url = "mongodb://localhost:27017/"
 
 const PORT = process.env.PORT || 5000
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use('/',movefiles)
+MongoClient.connect(url,(err,db) => {
+    if(err) throw err
+    console.log("connected successfully to the database")
+    var dbo = db.db("bookapi")
+    dbo.createCollection("customer",(err,res) => {
+        if(err) throw err
+        console.log("collection created!")
+        db.close()
+    })
+    
+})
+
+app.post("/addcustomer",(req,res) => {
+    const {name , adress} = req.body;
+    MongoClient.connect(url,(err,db) => {
+        if(err) throw err
+        var dbo = db.db("bookapi")
+        var obj = {
+            name,
+            adress
+        }
+        dbo.collection("customer").insertOne(obj,(err,res) => {
+            if(err) throw err
+            console.log("added data")
+            db.close()
+        })
+
+    })
+    res.status(200).send("1 inserted data")
+})
 
 app.listen(PORT,() => console.log(`the server is running on ${PORT}`))
--- backend/router/movefiles.js
+++ backend/router/movefiles.js
@@ -3,18 +3,23 @@ const router = express.Router()
 const fs = require('fs')
 const path = require('path')
 
-router.post('/move',(req,res) => {
-    const { oldpath } = req.body
+router.post('/addbook',(req,res) => {
+    const { namebook,authorbook, oldpath } = req.body
     const ext = path.extname(oldpath)
     if(ext != ".pdf") return res.status(400).send("Error : the extension must be pdf")
     const filename = path.basename(oldpath,'.pdf')
     var today = new Date()
     var year = today.getFullYear()
     var month = today.getMonth()
+    month = (month < 10)? "0"+month : month
     var day = today.getDay()
+    day = (day < 10)? "0"+day : day
     var hour = today.getHours()
+    hour = (hour < 10)? "0"+hour:hour
     var minute = today.getMinutes()
+    minute = (minute < 10)? "0"+minute:minute
     var second = today.getSeconds()
+    second = (second < 10)? "0"+second:second
     const newpath = `./files/${filename}${year}${month}${day}${hour}${minute}${second}.pdf`
     fs.copyFile(oldpath,newpath,(err) => {
         if(err) return res.status(400).send(`Error : ${err}`)
