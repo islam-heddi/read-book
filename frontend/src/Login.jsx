000bc0f5abc667213337e25fd1828008e65ab676
islam heddi <islamheddi19@gmail.com>
2024-08-12 14:00:02 +0100

    working on login form

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,6 +1,28 @@
 import NavBar from "./NavBar";
+import React,{useState} from 'react'
 
 function Login(){
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
+    const [labelbuttonPassword,setLabelButtonPassword] = useState("Show")
+    const [typePassword,settypePassword] = useState("password")
+    const handleEmail = (e) => {
+        setEmail(e.target.value)
+    }
+    const hanldePassword = (e) => {
+        setPassword(e.target.value)
+    }
+    const hanldeReset = (e) => {
+        e.preventDefault()
+        setEmail("")
+        setPassword("")
+    }
+    const ShowPassword = (e) => {
+        e.preventDefault()    
+        setLabelButtonPassword(prevLabel => prevLabel === "Show" ? "Hide" : "Show");
+        settypePassword(prevType => prevType === "password" ? "text" : "password"); 
+    }
+
     return(
         <>
             <NavBar />
@@ -10,13 +32,13 @@ function Login(){
                     <label>
                         Email : 
                     </label>
-                    <input type="text" placeholder="Enter your email"/><br />
+                    <input type="text" value={email} onChange={(e) =>  handleEmail(e)} placeholder="Enter your email"/><br />
                     <label>
                         Password :
                     </label>
-                    <input type="password" placeholder="************"/><br />
+                    <input type={typePassword} value={password} onChange={(e) => hanldePassword(e)} placeholder="************"/><button onClick={ShowPassword}>{labelbuttonPassword}</button><br />
                     <button>Validate</button>
-                    <button type="reset">Reset</button>
+                    <button onClick={hanldeReset}>Reset</button>
                 </form>
             </div>
         </>
